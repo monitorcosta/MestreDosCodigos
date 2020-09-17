f804e60b4e45a7186d5a9626b265a5301146a328
Ricardo Costa <ricardo.costa@db1.com.br>
2020-09-16 21:51:51 -0300

    Padronizado nome dos exercícios de teste

---
+++ TrabalhandoComTestesXUnit/CalculadorDeBhaskaraTests.cs
@@ -0,0 +1,33 @@
+using System;
+using TrabalhandoNoConsoleEx05;
+using Xunit;
+
+namespace TrabalhandoComTestesXUnit
+{
+    public class CalculadorDeBhaskaraTests
+    {
+        [Theory]
+        [InlineData(1, 8, -9, 1, -9)]
+        [InlineData(2, -16, -18, 9, -1)]
+        [InlineData(1, 12, -13, 1, -13)]
+        public void DeveCalcularAsRaizes(int coeficienteA, int coeficienteB, int coeficienteC, int raiz1, int raiz2)
+        {
+            var calculador = new CalculadorDeBhaskara(coeficienteA, coeficienteB, coeficienteC);
+
+            Assert.Equal(raiz1, calculador.CarlcularValorRaiz1());
+            Assert.Equal(raiz2, calculador.CarlcularValorRaiz2());
+        }
+
+        [Fact]
+        public void DeveValidarDeltaNegativo()
+        {
+            int coeficienteA = 7;
+            int coeficienteB = 3;
+            int coeficienteC = 4;
+
+            var calculador = new CalculadorDeBhaskara(coeficienteA, coeficienteB, coeficienteC);
+
+            Assert.True(calculador.DeltaNegativo());
+        }
+    }
+}
---
+++ TrabalhandoComTestesXUnit/ControleRemotoTests.cs
@@ -0,0 +1,56 @@
+﻿using UtilizandoPOOEx04;
+using Xunit;
+
+namespace TrabalhandoComTestesXUnit
+{
+    public class ControleRemotoTests
+    {
+        [Fact]
+        public void DeveAumentarOVolume()
+        {
+            var televisao = new Televisao();
+            var controleRemoto = new ControleRemoto(televisao);
+            var comando = "+";
+
+            controleRemoto.Controlar(comando);
+
+            Assert.Equal(2, televisao.Volume);
+        }
+
+        [Fact]
+        public void DeveDiminuirOVolume()
+        {
+            var televisao = new Televisao();
+            var controleRemoto = new ControleRemoto(televisao);
+            var comando = "-";
+
+            controleRemoto.Controlar(comando);
+
+            Assert.Equal(0, televisao.Volume);
+        }
+
+        [Fact]
+        public void DeveAumentarOCanal()
+        {
+            var televisao = new Televisao();
+            var controleRemoto = new ControleRemoto(televisao);
+            var comando = ">";
+
+            controleRemoto.Controlar(comando);
+
+            Assert.Equal(2, televisao.Canal);
+        }
+
+        [Fact]
+        public void DeveDiminuirOCanal()
+        {
+            var televisao = new Televisao();
+            var controleRemoto = new ControleRemoto(televisao);
+            var comando = "<";
+
+            controleRemoto.Controlar(comando);
+
+            Assert.Equal(0, televisao.Canal);
+        }
+    }
+}
